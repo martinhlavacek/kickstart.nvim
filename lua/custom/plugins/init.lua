9f3b2aad4c77abe667e839b91830aedd008bc838
Martin Hlavacek <martin@martinhlavacek.cz>
2023-03-28 21:20:09 +0200

    feat(custom): add vim tmux navigator and new my keymap

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,5 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-vim.opt.relativenumber = true
-vim.keymap.set("i", "jk", "<ESC>")
+
 return {}
---
+++ lua/custom/plugins/keymap.lua
@@ -0,0 +1,19 @@
+-- my keymaps
+
+
+local opt = vim.opt
+local keymap = vim.keymap
+
+opt.relativenumber = true
+keymap.set("i", "jk", "<ESC>")
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v")        -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s")        -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=")        -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>")    -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>")   -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>")     --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>")     --  go to previous tab
+return {}
